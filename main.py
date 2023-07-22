4ff6728e2307211d8c1af2fb8c7e3c98cba568ff
Ahmad Arshad <ahmadarshad12345@gmail.com>
2023-07-22 21:24:14 +0500

    today

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import tkinter as tk
 from tkinter import filedialog
-from tkcalendar import calendar
+from tkcalendar import DateEntry
 import ntplib
-def get_current_time():
-    ntp_client = ntplib.NTPClient()
-    response = ntp_client.request('pool.ntp.org')
+from datetime import datetime
 
-    if response:
-        return datetime.fromtimestamp(response.tx_time)
-    else:
-        return datetime.now()
+def get_current_date_string():
+    try:
+        ntp_client = ntplib.NTPClient()
+        response = ntp_client.request('pool.ntp.org')
+        current_date = datetime.fromtimestamp(response.tx_time)
+        date_string = current_date.strftime("%d--%m--%Y")
+        return date_string
+    except ntplib.NTPException:
+        return "Error: Unable to fetch current date from NTP server."
+
 def zip_window():
     location = filedialog.askdirectory()
     if location:
         zipwindow = tk.Toplevel(root)
         location_label = tk.Label(zipwindow, text=location, bg='green')
-        selectiondate = alendar(zipwindow, selectmode="day")
+        selectiondate = DateEntry(zipwindow)
+        today_date = tk.Label(zipwindow, text=get_current_date_string())
+
+        
 # now lets import date and time from the internet
-        print(get_current_time)
         location_label.pack(padx=5, pady=5)
         selectiondate.pack(padx=5, pady=5)
+        today_date.pack(padx=10, pady=10)
 
 def unzip_window():
     print("unzip")
--- maino.py
+++ maino.py
@@ -1,140 +1,70 @@
-import os
 import tkinter as tk
 from tkinter import filedialog
-from tkcalendar import Calendar
-import zipfile
+from tkcalendar import DateEntry
 from datetime import datetime, timedelta
 import ntplib
 
-expiration_time_var = None  # Global variable to hold the expiration time
-
-def zip_folder():
-    folder_path = filedialog.askdirectory()
-    if folder_path:
-        # Open the second window
-        second_window(folder_path)
-
-def second_window(folder_path):
-    global expiration_time_var  # Make sure to access the global variable
-
-    def set_unlock_date():
-        cal_date = cal.get_date()
-        expiration_time_var.set(cal_date.strftime("%m/%d/%y"))
-        cal_label.config(text="Selected Unlock Date: " + expiration_time_var.get())
-        cal.destroy()
-
-    def zip_and_lock_folder():
-        unlock_date_str = expiration_time_var.get()
-        unlock_date = datetime.strptime(unlock_date_str, "%m/%d/%y")
-        current_time = get_current_time()
-
-        if current_time < unlock_date:
-            lock_folder(folder_path, unlock_date)
-        else:
-            print("Unlock date must be in the future.")
-
-    def lock_folder(folder_path, unlock_date):
-        # Zip the folder and set a password
-        zip_file_name = f"{folder_path}_{unlock_date.strftime('%y-%m-%d')}.zip"
-        lock_info_file = os.path.join(folder_path, "lock_info.txt")
-
-        with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
-            for root, _, files in os.walk(folder_path):
-                for file in files:
-                    zipf.write(os.path.join(root, file))
-
-        # Create a lock_info.txt file with creation date and unlock date
-        with open(lock_info_file, 'w') as lock_info:
-            lock_info.write(f"Creation Date: {get_current_time().strftime('%Y-%m-%d %H:%M:%S')}\n")
-            lock_info.write(f"Unlock Date: {unlock_date.strftime('%Y-%m-%d %H:%M:%S')}")
-
-        # Save the unlock date in a file or database
-        # For simplicity, we'll just print it for now.
-        print(f"Folder locked until: {unlock_date}")
-
-    def get_current_time():
+def get_current_date_string():
+    try:
         ntp_client = ntplib.NTPClient()
         response = ntp_client.request('pool.ntp.org')
+        current_date = datetime.fromtimestamp(response.tx_time)
+        date_string = current_date.strftime("%d--%m--%Y")
+        return date_string
+    except ntplib.NTPException:
+        return "Error: Unable to fetch current date from NTP server."
 
-        if response:
-            return datetime.fromtimestamp(response.tx_time)
-        else:
-            return datetime.now()
+def zip_window():
+    def set_unlock_date():
+        unlock_date = selectiondate.get_date()
+        zip_and_lock_folder(location, unlock_date)
 
-    second_window = tk.Toplevel(root)
-    second_window.title("Zip and Lock Folder")
+    location = filedialog.askdirectory()
+    if location:
+        zipwindow = tk.Toplevel(root)
+        location_label = tk.Label(zipwindow, text=location, bg='green')
+        selectiondate = DateEntry(zipwindow)
+        date_today = tk.Label(zipwindow, text=get_current_date_string)
+        location_label.pack(padx=5, pady=5)
+        selectiondate.pack(padx=5, pady=5)
+        date_today.pack(padx=10, pady=10)
+        # Button to set the unlock date
+        unlock_date_button = tk.Button(zipwindow, text="Set Unlock Date", command=set_unlock_date)
+        unlock_date_button.pack()
 
-    # Display selected folder path
-    selected_folder_label = tk.Label(second_window, text="Selected Folder:")
-    selected_folder_label.pack()
-    selected_folder_path_label = tk.Label(second_window, text=folder_path)
-    selected_folder_path_label.pack()
+def zip_and_lock_folder(folder_path, unlock_date):
+    # Zip the folder and set a password
+    zip_file_name = f"{folder_path}_{unlock_date.strftime('%y-%m-%d')}.zip"
+    lock_info_file = f"{folder_path}_lock_info.txt"
 
-    # Calendar for setting the unlock date
-    cal = Calendar(second_window, selectmode="day")
-    cal.pack(pady=10)
+    with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                zipf.write(os.path.join(root, file))
 
-    # Button to set the unlock date
-    unlock_date_button = tk.Button(second_window, text="Set Unlock Date", command=set_unlock_date)
-    unlock_date_button.pack()
+    # Create a lock_info.txt file with creation date and unlock date
+    with open(lock_info_file, 'w') as lock_info:
+        lock_info.write(f"Creation Date: {get_current_time().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        lock_info.write(f"Unlock Date: {unlock_date.strftime('%Y-%m-%d %H:%M:%S')}")
 
-    # Label to display the selected unlock date
-    cal_label = tk.Label(second_window, text="")
-    cal_label.pack()
+    # Save the unlock date in a file or database
+    # For simplicity, we'll just print it for now.
+    print(f"Folder locked until: {unlock_date}")
 
-    # Zip and lock button
-    zip_button = tk.Button(second_window, text="Zip and Lock", command=zip_and_lock_folder)
-    zip_button.pack()
+def unzip_window():
+    print("unzip")
 
-# Function to unzip the folder if the unlock date has arrived
-def unzip_folder():
-    zip_file_path = filedialog.askopenfilename(filetypes=[("Zip Files", "*.zip")])
-    if zip_file_path:
-        unlock_date_str = zip_file_path.split('_')[-1].split('.')[0]
-        unlock_date = datetime.strptime(unlock_date_str, "%y-%m-%d")
-        current_time = get_current_time()
-
-        if current_time >= unlock_date:
-            unzip_folder_with_password(zip_file_path)
-        else:
-            print("Unlock date not reached.")
-
-def unzip_folder_with_password(zip_file_path):
-    # Read the lock_info.txt file to check unlock date
-    folder_path = os.path.dirname(zip_file_path)
-    lock_info_file = os.path.join(folder_path, "lock_info.txt")
-
-    with open(lock_info_file, 'r') as lock_info:
-        lock_info_lines = lock_info.readlines()
-        creation_date_str = lock_info_lines[0].split(': ')[1].strip()
-        unlock_date_str = lock_info_lines[1].split(': ')[1].strip()
-
-    creation_date = datetime.strptime(creation_date_str, "%Y-%m-%d %H:%M:%S")
-    unlock_date = datetime.strptime(unlock_date_str, "%Y-%m-%d %H:%M:%S")
-    current_time = get_current_time()
-
-    if current_time >= unlock_date:
-        # Unzip the folder
-        with zipfile.ZipFile(zip_file_path, 'r') as zipf:
-            zipf.extractall(folder_path)
-        print("Folder successfully unzipped.")
-    else:
-        print("Unlock date not reached.")
-
-# Main window
+# Our main window and its widgets
 root = tk.Tk()
-root.title("Zip and Unzip Program")
+root.geometry("150x150")
+root.title("Lock folder app")
 
-# Zip button
-zip_button = tk.Button(root, text="Zip", command=zip_folder)
-zip_button.pack(pady=20)
-
-# Unzip button
-unzip_button = tk.Button(root, text="Unzip", command=unzip_folder)
-unzip_button.pack(pady=20)
-
-# Exit button
-exit_button = tk.Button(root, text="Exit", command=root.quit)
-exit_button.pack(pady=20)
+# Main buttons
+zip_b = tk.Button(root, text="Zip", command=zip_window)
+unzip_b = tk.Button(root, text="Unzip", command=unzip_window)
+exit_b = tk.Button(root, text="Exit", bg='red', command=root.quit)
+zip_b.grid(row=0, column=0, padx=10, pady=20)
+unzip_b.grid(row=0, column=1, padx=10, pady=20)
+exit_b.grid(row=1, column=1, padx=10, pady=20)
 
 root.mainloop()
