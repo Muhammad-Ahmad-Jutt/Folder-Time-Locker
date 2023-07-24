7ce7c05b1e93c0143ffe84dde4908f996d557d3e
Ahmad Arshad <ahmadarshad12345@gmail.com>
2023-07-24 21:09:36 +0500

    Final

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkcalendar import DateEntry
-import ntplib, os, pyzipper
+import ntplib, os, pyzipper, base64
 from tqdm import tqdm
 from datetime import datetime
 import pandas as pd
+
+def alter_string(input_string):
+    # Encode the string using Base64 encoding
+    input_string = str(input_string)
+    altered_string = base64.b64encode(input_string.encode('utf-8')).decode('utf-8')
+    altered_string = str(altered_string)
+    return altered_string
+
+def reverse_altered_string(altered_string):
+    # Decode the Base64 encoded string to get the original string
+    original_string = base64.b64decode(altered_string).decode('utf-8')
+    return original_string
+
 def get_current_date_string():
     try:
         ntp_client = ntplib.NTPClient()
@@ -12,8 +25,12 @@ def get_current_date_string():
         current_date = datetime.fromtimestamp(response.tx_time)
         date_string = current_date.strftime("%Y-%m-%d")
         return date_string
-    except :
-        return "2023-07-23"
+    except:
+        # If there's an error with NTP server or fetching the time, fallback to system date
+        current_date = datetime.now()
+        date_string = current_date.strftime("%Y-%m-%d")
+        return date_string
+
 def count_numbers(s):
     num_str = ""
     for char in s:
@@ -24,12 +41,12 @@ def count_numbers(s):
     return num_str if num_str else 0
 
 def unzip_file_with_password(zip_file_path, password):
+    password = alter_string(password)
     try:
         with pyzipper.AESZipFile(zip_file_path, 'r', encryption=pyzipper.WZ_AES) as zipf:
             zip_file_name = os.path.splitext(os.path.basename(zip_file_path))[0]
             output_folder = os.path.join(os.path.dirname(zip_file_path), zip_file_name)
             os.makedirs(output_folder, exist_ok=True)
-
             zipf.setpassword(password.encode())
             zipf.extractall(path=output_folder)
 
@@ -41,12 +58,9 @@ def unzip_file_with_password(zip_file_path, password):
 
 
 
-
-
-
-
 def zip_file_or_folder(location, password, output_zip_file_name):
     # Check if the input is a file or a folder
+    password = alter_string(password)
     if os.path.isfile(location):
         # Compress a single file
         file_name = os.path.basename(location)
@@ -69,24 +83,15 @@ def zip_file_or_folder(location, password, output_zip_file_name):
         print("Invalid input. The location must be a valid file or folder.")
 
 
-
 def zipfile(location, s_date, t_date):
     s_date = s_date.get_date()
     t_date = t_date["text"]
     t_date = datetime.strptime(t_date, "%Y-%m-%d").date()
-    duration = s_date - t_date
-    duration = count_numbers(str(duration))
-    
     #selection date must be greater then the today date
-    if s_date >= t_date:
+    if s_date > t_date :
         #Frist we will set the name of the file 
         start_date_str = s_date.strftime("%Y-%m-%d")
         today_date_str = t_date.strftime("%Y-%m-%d")
-        file_name = f"Locked_From_{today_date_str}_To_{start_date_str}"
-        # to remember when the file will be unlocked
-        with open('date', 'w') as file:
-            file.write(file_name)
-        file.close
         try:
             s_date = datetime.strftime(s_date, "%Y-%m-%d")
             zip_file_or_folder(location, s_date, "output.zip")
@@ -95,7 +100,7 @@ def zipfile(location, s_date, t_date):
     else:
         print("invalid date")
 def zip_file():
-    location = filedialog.askopenfile()
+    location = filedialog.askopenfilename()
     zip_data(location)
 def zip_folder():
     location = filedialog.askdirectory()
@@ -107,6 +112,9 @@ def zip_data(location):
         selectiondate = DateEntry(zipwindow, date_pattern='dd-mm-yyyy')
         today_date = tk.Label(zipwindow, text=get_current_date_string())
         status_l = tk.Label(zipwindow, text=" ")
+
+        # endate = str(selectiondate)
+        # encrpt_password = tk.Button(zipwindow, text="Encrpy password", command=alter_string(endate))
         start_b = tk.Button(zipwindow, text='Start Compression', command=lambda : zipfile(location, selectiondate, today_date))
         cancel_b = tk.Button(zipwindow, text='Cancel', command=zipwindow.destroy)
 # now lets import date and time from the internet
@@ -114,6 +122,7 @@ def zip_data(location):
         selectiondate.pack(padx=5, pady=5)
         today_date.pack(padx=10, pady=10)
         status_l.pack(padx=10, pady=10)
+        # encrpt_password.pack(padx=10, pady=10)
         start_b.pack(padx=10, pady=10)
         cancel_b.pack(padx=10, pady=10)
 def unzip_window():
